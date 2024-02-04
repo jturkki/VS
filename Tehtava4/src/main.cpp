5d888671c8e941061ac7fd1bc8d6337751d4e088
jturkki <jyri.turkki@gmail.com>
2024-02-04 17:13:02 +0200

    Lisätty kommentteja tehtävään 4

--- Tehtava4/src/main.cpp
+++ Tehtava4/src/main.cpp
@@ -20,25 +20,25 @@ void setup()
 
 void loop() 
 {
-  digitalWrite(pwrNTC, LOW);
+  digitalWrite(pwrNTC, LOW);        // varmistetaan että sensorissa ei ole virrat päällä
   analogReadResolution(10);
-  if ((!digitalRead(buttonB1)) && (a == 0))
+  if ((!digitalRead(buttonB1)) && (a == 0))   // kun nappia painetaan, a varmistaa että tämä tehdään vain kerran
   {
-    Serial.println("aloitus");
-    digitalWrite(pwrNTC, HIGH);
-    alkuAika = micros();
+    Serial.println("aloitus");        // debuggausta varten 
+    digitalWrite(pwrNTC, HIGH);       // virrat päälle sensoriin
+    alkuAika = micros();              // otetaan aloitusaika talteen
 
     for (int i=0; i<50; i++)
     {
-      mittaukset[i] = analogRead(sensoriNTC);
+      mittaukset[i] = analogRead(sensoriNTC);       // luetaan 50 mittausta taulukkoon
       delay(10);
     }
-    loppuAika = micros();
-    a = 1;
+    loppuAika = micros();             // loppuaika talteen
+    a = 1;                            // a varmistaa että mittaukset tehdään vain kerran
 
     for (int i=0; i<50; i++)
     {
-      Serial.print(mittaukset[i]);
+      Serial.print(mittaukset[i]);      // tulostetaan mittaukset
       Serial.print("; ");
     }
     Serial.println(" ");
